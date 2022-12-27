4ff2999f4ccf355c510ab0d254d046baa361cd0b
seodaekyu <grarlove7@naver.com>
2022-12-27 15:56:27 +0900

    Spring04 에 Transaction 관리예제 추가함

--- Spring04_Final/src/main/java/com/gura/spring04/shop/service/ShopServiceImpl.java
+++ Spring04_Final/src/main/java/com/gura/spring04/shop/service/ShopServiceImpl.java
@@ -6,8 +6,10 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.gura.spring04.exception.DeliveryException;
 import com.gura.spring04.shop.dao.OrderDao;
 import com.gura.spring04.shop.dao.ShopDao;
 import com.gura.spring04.shop.dto.OrderDto;
@@ -26,20 +28,37 @@ public class ShopServiceImpl implements ShopService{
 		// ModelAndView 객체에 list 라는 키 값으로 담는다.
 		mView.addObject("list", list);
 	}
-
+	
+	/*
+	 * 	- spring 트랜젝션 설정 방법
+	 * 	1. pom.xml 에 spring-tx dependency 추가
+	 * 	2. servlet-context.xml 에 transaction 설정 추가
+	 * 	3. 트랜젝션을 관리할 서비스의 메소드에 @Transactional 어노테이션 붙이기
+	 * 
+	 * 	- 프로그래머의 의도하에서 트랜젝션에 영향을 주는 Exception 을 발생시키는 방법
+	 * 
+	 * 	ex) class MyException extends DataAccessException{ }
+	 * 
+	 * 		throw new MyException("예외 메시지");
+	 * 
+	 * 	예외를 발생시킬 조건이라면 위와 같이 예외를 발생시켜서
+	 * 	트랜젝션이 관리되도록 한다.
+	 */
+	
+	@Transactional
 	@Override
 	public void buy(HttpServletRequest request, ModelAndView mView) {
 	    // 구입자의 아이디
-        String id=(String)request.getSession().getAttribute("id");
+        String id = (String)request.getSession().getAttribute("id");
         
         // 1. 파라미터로 전달되는 구입할 상품 번호
-        int num=Integer.parseInt(request.getParameter("num"));
+        int num = Integer.parseInt(request.getParameter("num"));
         
         // 2. 상품의 가격을 얻어온다.
-        int price=shopDao.getPrice(num);
+        int price = shopDao.getPrice(num);
         
         // 3. 상품의 가격만큼 계좌 잔액을 줄인다.
-        ShopDto dto=new ShopDto();
+        ShopDto dto = new ShopDto();
         dto.setId(id);
         dto.setPrice(price);
         shopDao.minusMoney(dto);
@@ -51,12 +70,18 @@ public class ShopServiceImpl implements ShopService{
         shopDao.minusCount(num);
         
         // 6. 주문 테이블(배송) 에 정보를 추가 한다. 
-        OrderDto dto2=new OrderDto();
+        OrderDto dto2 = new OrderDto();
         dto2.setId(id); // 누가
         dto2.setCode(num); // 어떤 상품을 
         // 클라이언트가 입력한 배송 주소라고 가정 
-        String addr="서울시 강남구 삼원타워";
-        dto2.setAddr(addr);// 어디로 배송할지
+        String addr="제주도 삼원타워";
+        
+        // 가상의 테스트
+        if(addr.contains("제주도")) {
+        	throw new DeliveryException("제주도는 배송 불가 지역입니다.");
+        }
+        
+        dto2.setAddr(addr); // 어디로 배송할지
         orderDao.addOrder(dto2);
 
 		
