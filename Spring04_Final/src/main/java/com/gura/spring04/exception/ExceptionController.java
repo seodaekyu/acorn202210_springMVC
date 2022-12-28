ff264eaa82f2bf6ed4be60697058c6927f183970
seodaekyu <grarlove7@naver.com>
2022-12-28 15:30:12 +0900

    Spring04 에 Gallery 기능 구현함

--- Spring04_Final/src/main/java/com/gura/spring04/exception/ExceptionController.java
+++ Spring04_Final/src/main/java/com/gura/spring04/exception/ExceptionController.java
@@ -7,7 +7,9 @@ import org.springframework.web.servlet.ModelAndView;
 /*
  * 	Spring MVC 가 동작중에 특정 type 의 예외가 발생하면 해당 예외를 여기서 처리할 수 있다.
  */
-@ControllerAdvice
+
+  @ControllerAdvice
+
 public class ExceptionController {
 	
 	// spring framework 가 동작하는 중에 NotDeleteException type 의
