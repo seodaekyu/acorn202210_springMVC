64646c9ccaab59e74e1394df5b5979269ad7ff8b
seodaekyu <grarlove7@naver.com>
2022-12-29 10:04:06 +0900

    Boot04 에 필기 추가함

--- Boot04_MyBatis/src/main/java/com/gura/boot04/member/dao/MemberDao.java
+++ Boot04_MyBatis/src/main/java/com/gura/boot04/member/dao/MemberDao.java
@@ -6,10 +6,14 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+// 1. @RestController 를 이용해서 Controller 를 만들고
 @Repository
 public class MemberDao {
 	
 	@Autowired SqlSession session;
+	
+	// 2. 메소드에서 어떤 data 를 리턴하면 리턴되는 데이터가 클라이언트에게 바로 응답된다.
+	// String 은 내용 그대로 응답되고, dto, Map, List 등은 JSON 문자열로 변환되어 응답된다.
 	
 	public List<MemberDto> getList(){
 		return session.selectList("member.getList");
