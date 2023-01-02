0a3633762d484a01c7654efdea5c6db1a488d8d7
seodaekyu <grarlove7@naver.com>
2023-01-02 11:22:11 +0900

    Boot07_Final 에 users 관련 기능 추가

--- Boot07_Final/src/main/java/com/gura/boot07/config/WebConfig.java
+++ Boot07_Final/src/main/java/com/gura/boot07/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.gura.boot07.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.gura.boot07.interceptor.LoginInterceptor;
@@ -16,10 +17,18 @@ public class WebConfig implements WebMvcConfigurer{
 	// 인터셉터 동작하도록 등록하기
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
+		
 		registry.addInterceptor(loginInterceptor)
 		.addPathPatterns("/users/*")
 		.excludePathPatterns("/users/signup_form", "/users/signup", "/users/loginform", "/users/login");
 	}
 	
+	// resources 폴더안에 있는 자원을 spring 컨트롤러를 거치지 않고 응답되도록 설정
+	// webapp 안에 resources 폴더를 만들어야 한다.
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
 	
 }
