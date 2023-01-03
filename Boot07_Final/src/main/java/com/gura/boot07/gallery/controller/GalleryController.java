d97de9d26d6dfa25f7092730077aef8ad41285ee
seodaekyu <grarlove7@naver.com>
2023-01-03 10:20:58 +0900

    Boot07 에 custom.properties 추가 및 일부 수정

--- Boot07_Final/src/main/java/com/gura/boot07/gallery/controller/GalleryController.java
+++ Boot07_Final/src/main/java/com/gura/boot07/gallery/controller/GalleryController.java
@@ -38,7 +38,16 @@ public class GalleryController {
 	// 프로필 이미지 요청에 대한 응답을 할 메소드를 따로 만들어야 한다.
 	// 이미지 데이터가 응답되어야 한다.
 	// 웹브라우저에게 이미지 데이터를 응답한다고 알려야 한다.
-	// 응답할 이미지의 이름은 그때 그때 다르다.
+	// 응답할 이미지의 이름은 그때 그때 다르다. 그렇기때문에 { } 중괄호 사용
+	
+	/*
+	 * 	이 컨트롤러 메소드에서 응답한 byte[] 배열을 클라이언트에게 응답하는 방법
+	 * 	1. @ResponseBody
+	 * 	2. byte[] 배열 리턴
+	 * 
+	 * 	응답된 byte[] 배열에 있는 데이터를 이미지 데이터로 클라이언트 웹브라우저가 인식하게 하는 방법
+	 * 	produces = MediaType.IMAGE_JPEG_VALUE
+	 */
 	@GetMapping(
 			value="/gallery/images/{imageName}",
 			produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_GIF_VALUE}
@@ -49,7 +58,7 @@ public class GalleryController {
 		String absolutPath = fileLocation+File.separator+imageName;
 		// 파일에서 읽어들일 InputStream
 		InputStream is = new FileInputStream(absolutPath);
-		
+		// 이미지데이터(byte) 를 읽어서 배열에 담아 클라이언트에게 응답
 		return IOUtils.toByteArray(is);
 	}
 
