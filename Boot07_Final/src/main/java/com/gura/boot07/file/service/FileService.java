8413025cd5afdff89e97d2ffb68803e9bb302c11
seodaekyu <grarlove7@naver.com>
2023-01-03 11:28:38 +0900

    Boot07 에 자료실기능 수정

--- Boot07_Final/src/main/java/com/gura/boot07/file/service/FileService.java
+++ Boot07_Final/src/main/java/com/gura/boot07/file/service/FileService.java
@@ -8,14 +8,14 @@ import com.gura.boot07.file.dto.FileDto;
 
 public interface FileService {
 	   
-	   //파일 목록 얻어오기 
+	   // 파일 목록 얻어오기 
 	   public void getList(HttpServletRequest request);
-	   //업로드된 파일 저장하기 
+	   // 업로드된 파일 저장하기 
 	   public void saveFile(FileDto dto, ModelAndView mView,
 			   			HttpServletRequest request);
-	   //파일하나의 정보 얻어오기 
-	   public void getFileData(int num, ModelAndView mView);
-	   //파일 삭제하기
+	   // 파일하나의 정보 얻어오기 
+	   public FileDto getFileData(int num);
+	   // 파일 삭제하기
 	   public void deleteFile(int num, HttpServletRequest request);
    
    
